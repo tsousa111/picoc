42f4d0e211d4cedea22bb7e7213c904c35fda14b
tsousa111 <tiagao2001@hotmail.com>
2024-05-25 18:58:17 +0100

    fix runpicoc, add return statement, fix if parsing

--- PicoC.hs
+++ PicoC.hs
@@ -6,9 +6,11 @@
 
 module PicoC where
 
+import Debug.Trace
 import Data.Char
 import Data.Data
 import Data.Generics.Zipper
+import Data.Maybe
 import Library.StrategicData (StrategicData)
 import Library.Ztrategic
 import Parser
@@ -20,12 +22,14 @@ instance StrategicData PicoC
 instance (StrategicData a) => StrategicData [a]
 
 data PicoC = PicoC [Inst]
-    deriving (Data, Eq)
+    deriving (Data, Eq, Show)
 
 data Inst
     = Attrib !String !Exp
     | While !Exp !CBlock
     | ITE !Exp !CBlock !CBlock
+    | Return !Exp
+    | Print !String
     deriving (Show, Data, Eq)
 
 type CBlock = [Inst]
@@ -71,99 +75,54 @@ data Exp
 pExp = pExp5
 
 pExp5 :: Parser Exp
-pExp5 =
-    f
-        <$> pExp4
-        <*> token' "||"
-        <*> pExp5
-        <|> pExp4
+pExp5 = f <$> pExp4 <*> token' "||" <*> pExp5
+     <|> pExp4
   where
     f x _ = OR x
 
 pExp4 :: Parser Exp
-pExp4 =
-    f
-        <$> pExp3
-        <*> token' "&&"
-        <*> pExp4
-        <|> pExp3
+pExp4 = f <$> pExp3 <*> token' "&&" <*> pExp4
+     <|> pExp3
   where
     f x _ = AND x
 
 pExp3 :: Parser Exp
-pExp3 =
-    f
-        <$> pExp2
-        <*> token' "=="
-        <*> pExp3
-        <|> pExp2
+pExp3 = f <$> pExp2 <*> token' "==" <*> pExp3
+     <|> pExp2
   where
     f x _ = EQ x
 
 pExp2 :: Parser Exp
-pExp2 =
-    f
-        <$> pExp1
-        <*> token' "<"
-        <*> pExp2
-        <|> g
-        <$> pExp1
-        <*> token' ">"
-        <*> pExp2
-        <|> pExp1
+pExp2 = f <$> pExp1 <*> token' "<" <*> pExp2
+     <|> g <$> pExp1 <*> token' ">" <*> pExp2
+     <|> pExp1
   where
     f x _ = LT x
     g x _ = GT x
 
 pExp1 :: Parser Exp
-pExp1 =
-    f
-        <$> pExp0
-        <*> symbol' '+'
-        <*> pExp1
-        <|> g
-        <$> pExp0
-        <*> symbol' '-'
-        <*> pExp1
-        <|> pExp0
+pExp1 = f <$> pExp0 <*> symbol' '+' <*> pExp1
+     <|> g <$> pExp0 <*> symbol' '-' <*> pExp1
+     <|> pExp0
   where
     f x _ = Add x
     g x _ = Sub x
 
 pExp0 :: Parser Exp
-pExp0 =
-    f
-        <$> pFactor
-        <*> symbol' '*'
-        <*> pExp0
-        <|> g
-        <$> pFactor
-        <*> symbol' '/'
-        <*> pExp0
-        <|> pFactor
+pExp0 = f <$> pFactor <*> symbol' '*' <*> pExp0
+     <|> g <$> pFactor <*> symbol' '/' <*> pExp0
+     <|> pFactor
   where
     f x _ = Mult x
     g x _ = Div x
 
-pFactor =
-    f
-        <$> pInt
-        <|> g
-        <$> pNames
-        <|> h
-        <$> token' "TRUE"
-        <|> i
-        <$> token' "FALSE"
-        <|> j
-        <$> symbol' '-'
-        <*> pFactor
-        <|> k
-        <$> symbol' '('
-        <*> pExp
-        <*> symbol' ')'
-        <|> l
-        <$> symbol' '!'
-        <*> pFactor
+pFactor = f <$> pInt
+       <|> g <$> pNames
+       <|> h <$> token' "TRUE"
+       <|> i <$> token' "FALSE"
+       <|> j <$> symbol' '-' <*> pFactor
+       <|> k <$> symbol' '!' <*> pFactor
+       <|> l <$> symbol' '(' <*> pExp <*> symbol' ')'
   where
     f s = Const $ case readMaybe s :: Maybe Int of
         Just x -> x
@@ -171,9 +130,9 @@ pFactor =
     g = Var
     h _ = TRUE
     i _ = FALSE
-    k _ x _ = x
-    l _ = Not
     j _ = Neg
+    k _ = Not
+    l _ x _ = x
 
 pAtrib :: Parser Inst
 pAtrib = f <$> oneOrMore (satisfy' isAlphaNum) <*> symbol' '=' <*> pExp <*> symbol' ';'
@@ -194,25 +153,19 @@ pWhile = f <$> token' "while" <*> pExp <*> pCBlock
 
 -- NOTE: a prioridade é de baixo para cima
 pIf :: Parser Inst
-pIf =
-    f
-        <$> token' "if"
-        <*> pExp
-        <*> token' "then"
-        <*> pCBlock
-        <|> g
-        <$> token' "if"
-        <*> pExp
-        <*> token' "then"
-        <*> pCBlock
-        <*> token' "else"
-        <*> pCBlock
+pIf = f <$> token' "if" <*> symbol' '(' <*> pExp <*>symbol' ')' <*> token' "then" <*> pCBlock <*> token' "else" <*> pCBlock
+   <|> g <$> token' "if" <*> symbol' '(' <*> pExp <*>symbol' ')' <*> token' "then" <*> pCBlock
   where
-    f _ x _ y = ITE x y []
-    g _ x _ y _ = ITE x y
+    f _ _ x _ _ y _ z = ITE x y z
+    g _ _ x _ _ y = ITE x y []
+
+pReturn :: Parser Inst
+pReturn = f <$> token' "return" <*> pExp <*> symbol' ';'
+  where
+    f _ x _ = Return x
 
 pInst :: Parser Inst
-pInst = pAtrib <|> pWhile <|> pIf
+pInst = pIf <|> pReturn <|> pWhile <|> pAtrib
 
 pPicoC :: Parser PicoC
 pPicoC = PicoC <$> oneOrMore pInst
@@ -224,8 +177,8 @@ picoC s = fst $ head $ filter ((== "") . snd) (pPicoC s)
 -- unparse : AST to text
 -- pretty printing
 ---------------------------
-instance Show PicoC where
-    show = unparse
+-- instance Show PicoC where
+--     show = unparse
 
 -- FIX: fix unparser
 unparse :: PicoC -> String
@@ -283,11 +236,11 @@ eval (Div x y) c = eval x c `div` eval y c
 eval (Neg x) c = -(eval x c)
 eval TRUE _ = 1
 eval FALSE _ = 0
+eval (LT e1 e2) c = if eval e1 c < eval e2 c then 1 else 0
+eval (GT e1 e2) c = if eval e1 c > eval e2 c then 1 else 0
+eval (EQ e1 e2) c = if eval e1 c == eval e2 c then 1 else 0
 eval (AND e1 e2) c = if (eval e1 c /= 0) && (eval e2 c /= 0) then 1 else 0
 eval (OR e1 e2) c = if (eval e1 c /= 0) || (eval e2 c /= 0) then 1 else 0
-eval (LT e1 e2) c = if (eval e1 c /= 0) < (eval e2 c /= 0) then 1 else 0
-eval (GT e1 e2) c = if (eval e1 c /= 0) > (eval e2 c /= 0) then 1 else 0
-eval (EQ e1 e2) c = if (eval e1 c /= 0) == (eval e2 c /= 0) then 1 else 0
 eval (Not e) c = if not (eval e c /= 0) then 1 else 0
 
 opt :: Exp -> Exp
@@ -400,66 +353,66 @@ ifMerge (ITE x1 [ITE x2 t2 []] e1) = Just (ITE (AND x1 x2) t2 e1)
 ifMerge _ = Nothing
 
 -- NOTE: Quickcheck Tpc Entrega 04-04 do prop
-instance Arbitrary PicoC where
-    arbitrary = genPicoC
-genPicoC :: Gen PicoC
-genPicoC = do
-    x <- vectorOf 5 genInst
-    return (PicoC x)
-
-genInst :: Gen Inst
-genInst = frequency [(95, genAttrib), (2, genWhile), (3, genITE)]
-
-genAttrib :: Gen Inst
-genAttrib = do
-    x <- genVarName
-    Attrib x <$> genExp
-
-genWhile :: Gen Inst
-genWhile = do
-    x <- genExp
-    While x <$> genCBlock
-
-genITE :: Gen Inst
-genITE = do
-    x <- genExp
-    y <- genCBlock
-    ITE x y <$> genCBlock
-
-genExp :: Gen Exp
-genExp = frequency [genConst, genVar, genBool, genArithmeticExpo, genAND, genOR, genLT, genGT, genEQ, genNot]
-  where
-    genConst = (60, do Const <$> arbitrary)
-    genVar = (30, do Var <$> genVarName)
-    genBool = (10, elements [TRUE, FALSE])
-    genArithmeticExpo = (1, genArithmeticExp)
-    genAND = (1, do x <- genExp; AND x <$> genExp)
-    genOR = (1, do x <- genExp; OR x <$> genExp)
-    genLT = (1, do x <- genExp; LT x <$> genExp)
-    genGT = (1, do x <- genExp; GT x <$> genExp)
-    genEQ = (1, do x <- genExp; EQ x <$> genExp)
-    genNot = (1, do Not <$> genExp)
-
-genArithmeticExp :: Gen Exp
-genArithmeticExp = frequency [genConst, genVar, genAdd, genSub, genMult, genDiv, genNeg]
-  where
-    genConst = (80, do Const <$> arbitrary)
-    genVar = (15, do Var <$> genVarName)
-    genAdd = (1, do x <- genArithmeticExp; Add x <$> genArithmeticExp)
-    genSub = (1, do x <- genArithmeticExp; Sub x <$> genArithmeticExp)
-    genMult = (1, do x <- genArithmeticExp; Mult x <$> genArithmeticExp)
-    genDiv = (1, do x <- genArithmeticExp; Div x <$> genArithmeticExp)
-    genNeg = (1, do Neg <$> genArithmeticExp)
-
-genVarName :: Gen String
-genVarName = do
-    -- to make sure the var name is valid
-    f <- elements ['a' .. 'z']
-    r <- listOf (elements (['a' .. 'z'] ++ ['A' .. 'Z'] ++ ['0' .. '9']))
-    return (f : r)
-
-genCBlock :: Gen CBlock
-genCBlock = vectorOf 5 genInst
+-- instance Arbitrary PicoC where
+--     arbitrary = genPicoC
+-- genPicoC :: Gen PicoC
+-- genPicoC = do
+--     x <- vectorOf 5 genInst
+--     return (PicoC x)
+--
+-- genInst :: Gen Inst
+-- genInst = frequency [(95, genAttrib), (2, genWhile), (3, genITE)]
+--
+-- genAttrib :: Gen Inst
+-- genAttrib = do
+--     x <- genVarName
+--     Attrib x <$> genExp
+--
+-- genWhile :: Gen Inst
+-- genWhile = do
+--     x <- genExp
+--     While x <$> genCBlock
+--
+-- genITE :: Gen Inst
+-- genITE = do
+--     x <- genExp
+--     y <- genCBlock
+--     ITE x y <$> genCBlock
+--
+-- genExp :: Gen Exp
+-- genExp = frequency [genConst, genVar, genBool, genArithmeticExpo, genAND, genOR, genLT, genGT, genEQ, genNot]
+--   where
+--     genConst = (60, do Const <$> arbitrary)
+--     genVar = (30, do Var <$> genVarName)
+--     genBool = (10, elements [TRUE, FALSE])
+--     genArithmeticExpo = (1, genArithmeticExp)
+--     genAND = (1, do x <- genExp; AND x <$> genExp)
+--     genOR = (1, do x <- genExp; OR x <$> genExp)
+--     genLT = (1, do x <- genExp; LT x <$> genExp)
+--     genGT = (1, do x <- genExp; GT x <$> genExp)
+--     genEQ = (1, do x <- genExp; EQ x <$> genExp)
+--     genNot = (1, do Not <$> genExp)
+--
+-- genArithmeticExp :: Gen Exp
+-- genArithmeticExp = frequency [genConst, genVar, genAdd, genSub, genMult, genDiv, genNeg]
+--   where
+--     genConst = (80, do Const <$> arbitrary)
+--     genVar = (15, do Var <$> genVarName)
+--     genAdd = (1, do x <- genArithmeticExp; Add x <$> genArithmeticExp)
+--     genSub = (1, do x <- genArithmeticExp; Sub x <$> genArithmeticExp)
+--     genMult = (1, do x <- genArithmeticExp; Mult x <$> genArithmeticExp)
+--     genDiv = (1, do x <- genArithmeticExp; Div x <$> genArithmeticExp)
+--     genNeg = (1, do Neg <$> genArithmeticExp)
+--
+-- genVarName :: Gen String
+-- genVarName = do
+--     -- to make sure the var name is valid
+--     f <- elements ['a' .. 'z']
+--     r <- listOf (elements (['a' .. 'z'] ++ ['A' .. 'Z'] ++ ['0' .. '9']))
+--     return (f : r)
+--
+-- genCBlock :: Gen CBlock
+-- genCBlock = vectorOf 5 genInst
 
 -- NOTE: Check if a PicoC code is valid
 prop_valid :: PicoC -> Bool
@@ -476,28 +429,59 @@ prop_optimization_idempotent p = arithmeticOpt p == arithmeticOpt (arithmeticOpt
 -- NOTE: Evaluate
 type Inputs = [(String, Int)]
 
-input1 = [("a", 1), ("b", 2), ("c", 3)]
 
-removeItem :: a -> [(a, b)] -> [(a, b)]
+removeItem :: (Eq a) => a -> [(a, b)] -> [(a, b)]
 removeItem _ [] = []
 removeItem k (c@(ck, v) : t)
     | k == ck = t
     | otherwise = c : removeItem k t
 
--- TODO: Make a return statment
+prog1 = picoC "if (a > 3) then { a = a + 1; }else { a = b; } return a;"
+inputs1 :: Inputs
+inputs1 = [("a", 2), ("b", 5)]
+
+prog2 = picoC "while (a < 5) { a = a + 1;} return b;"
+inputs2 :: Inputs
+inputs2 = [("a", 0), ("b", 5)]
+
+prog3 = picoC "if (!(a > b)) then { if (b > c) then { m = b; } else { m = c; } } else { if (a > c) then { m = a; } else { m = b; } } return m;"
+inputs3 :: Inputs
+inputs3 = [("a", 1), ("b", 5),("c", 3)]
+
+prog4 = picoC "if ((a > 3) > (b < 4)) then { a = 1;} a = 5; b = 2; return a;"
+prog5 = picoC "a = (a > 3) > (b < 4);"
 evaluate :: PicoC -> Inputs -> Int
-evaluate (PicoC i) inp = runPicoC i inp
+evaluate (PicoC i) inp = ret
+  where
+   (c, Just ret) = runPicoC i inp
 
 -- NOTE: eval exp needs to work with logical statements
-runPicoC :: [Inst] -> Inputs -> Inputs
-runPicoC [] i = i
+runPicoC :: [Inst] -> Inputs -> (Inputs, Maybe Int)
+runPicoC [] i = (i, Nothing)
+runPicoC ((Return x) : _) i = (i, Just (eval x i))
 runPicoC ((Attrib n v) : t) i = runPicoC t ((n, eval v i) : removeItem n i)
 runPicoC ((ITE exp e1 e2) : t) i
-    | eval exp i /= 0 = runPicoC t (runPicoC e1 i)
-    | otherwise = runPicoC t (runPicoC e2 i)
+    | eval exp i /= 0 = if isJust ret_e2 then i_e2 else runPicoC t inputs_e1
+    | otherwise = if isJust ret_e2 then i_e2 else runPicoC t inputs_e2
+  where
+    i_e1@(inputs_e1, ret_e1) = runPicoC e1 i
+    i_e2@(inputs_e2, ret_e2) = runPicoC e2 i
 runPicoC inst@((While exp c) : t) i
-    | eval exp i /= 0 = runPicoC inst (runPicoC c i)
+    | eval exp i /= 0 = if isJust ret then i_while else runPicoC inst inp_while
     | otherwise = runPicoC t i
+  where
+    i_while@(inp_while, ret) = runPicoC c i
+
+runTest :: PicoC -> (Inputs, Int) -> Bool
+runTest p (i, result) = evaluate p i == result
+
+runTestSuite :: PicoC -> [(Inputs, Int)] -> Bool
+runTestSuite _ [] = True
+runTestSuite p (h:t) = runTest p h && runTestSuite p t
+
+
+
+
 
 -- Example
 examplePicoC = "margem = 15; \n if (!(margem > 3)) then { \n margem = 4 * 23 + 3 ; \n} else {\n margem = x * 1 + 0; \n}"
